5d7ca901cab331fff294ca3c4824f804f7d87105
Tishana <tishana.trainor@gmail.com>
2018-11-12 21:54:37 -0500

    added hbs template and index page

--- controllers/appController.js
+++ controllers/appController.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Users = require('../models/User')
 
-router.get('/', (req, res) => {
-    res.send('This is the root directory')
+router.get('/', (req, res) => {//homepage
+    res.render(index, { page: 'homepage' })
+})
+// router.use('/user')
+
+router.get('/:id', (req, res) => {
+    res.send('You are requesting to see a User')
 })
 
 module.exports = router
