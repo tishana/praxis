2bd997d64ec6f31d9da40bbbe21da33019b4a82d
Tishana <tishana.trainor@gmail.com>
2018-11-13 16:58:26 -0500

    still trying to show ONE goal

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -2,15 +2,17 @@ const Goal = require('../models/Goal')
 // const Habit = require('../models/Habit')
 module.exports = {
     show: (req, res) => {//show one goal
-        Goal.findOne({ _id: req.params.id }).then(goal => {
-            res.render('goal/show', {//show found goal on show layout
-                goal
-            })
-        })
+        console.log(`id ${req.params.id}`)
+        Goal.findById(req.params.id)
+            .then(goal => {
+                console.log(goal)
+            }
+            )
     }
+}
     // new: (req, res) => { },
     // create: (req, res) => { },
     // update: (req, res) => { },
     // delete: (req, res) => { },
     // show: (req, res) => { }
-}
+
