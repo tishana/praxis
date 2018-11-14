c0101fbbee63841223a8edb9b8da53993deb46d8
Tishana <tishana.trainor@gmail.com>
2018-11-14 16:44:19 -0500

    form works cannot get info from form

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,18 +1,36 @@
 const Goal = require('../models/Goal')
 // const Habit = require('../models/Habit')
 module.exports = {
-    show: (req, res) => {//show one goal
+    show: (req, res) => {//show one Goal
         // console.log(`id ${req.params.id}`)
-        Goal.findOne({ _id: req.params.id }).then(goal => {
-            res.render("goal/show", goal)
-        })
+        Goal.findById(req.params.id)
+            .then(goal => {
+                res.render('goal/show', goal)
+            })
+            .catch(err => {
+                console.log('error', err)
+            })
     },
-    new: (req, res) => {//form for a new goal
+    new: (req, res) => {//form for a new Goal
         res.render('goal/new')
-    }
+    },
+    create: (req, res) => {//creating a new Goal from form
+        Goal.create({
+            title: req.body.title,
+            description: req.body.description,
+            dateGoal: req.body.dateGoal
+        }).then(goal => {
+            res.redirect(`goal/${goal._id}`)
+        })
+    }//,
+    // update: (req, res) => {//adding a new habit
+    //     Goal.findById(req.params.id).then(goal => {
+    //         res.render('goal/update', goal)
+    //     })
+    // }
 }
-    // create: (req, res) => { },
-    // update: (req, res) => { },
+
+
     // delete: (req, res) => { },
     // show: (req, res) => { }
 
--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const goalController = require('../controllers/goalController')
 
+router.get('/new', goalController.new)
 router.get('/:id', goalController.show)
-router.get('/goal/new', goalController.new)
-// router.get('/goal/:id/edit', goalController.edit)
+router.post('/', goalController.create)
+// router.get('/:id/edit', goalController.edit)
 // router.get('/')
 
 module.exports = router
