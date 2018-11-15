8ef5b6234cba20efbb436a4fb71b0c3438f479ae
Tishana <tishana.trainor@gmail.com>
2018-11-15 15:17:45 -0500

    seeding issues pt 2

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,8 @@
 // the setup for the seed data for this project
-// const mongoose = require("./connection")
+const mongoose = require("./connection")
 const seeds = require("./seedData.json")
-const Goal = require("../models/Goal");
-// mongoose.Promise = Promise;
+const { Goal, Habit } = require('../models/Goal')
+mongoose.Promise = Promise
 // const { Habit } = require("../models/Habit");
 
 Goal.remove({})
