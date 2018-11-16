b976ef4212fd862d8ee661a71d5dc0dd59ae37da
Tishana <tishana.trainor@gmail.com>
2018-11-16 10:51:16 -0500

    Fixed crash

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
 // Controller for User to be added later
 
-// module.exports = {
-//     index: (req, res) => {
-//         res.render('../views/index', { page: 'homepage' })
-//     }
-//     // new: (req, res) => { },
-//     // create: (req, res) => { }
+module.exports = {
+    index: (req, res) => {
+        res.render('../views/index', { page: 'homepage' })
+    }
+    // new: (req, res) => { },
+    // create: (req, res) => { }
 
-// }
+}
